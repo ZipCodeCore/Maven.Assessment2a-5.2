88916f6770dd808f55eab1857ca0557226760899
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-11-11 12:00:29 -0500

    collections finished

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,15 +1,24 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Use a map to keep track of inventory in a store
  */
+
 public class Inventory {
+
+    Map<String, Integer> inventory;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
+        inventory = new HashMap<>();
+
+        for(String thing : strings)
+            inventory.put(thing, 1);
 
     }
 
@@ -18,20 +27,34 @@ public class Inventory {
      */
     public Inventory() {
 
+        inventory = new HashMap<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+
+        if (!inventory.containsKey(item))
+            inventory.put(item, 1);
+        else {
+            for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+                if (entry.getKey().equals(item))
+                    inventory.replace(entry.getKey(), entry.getValue() + 1);
+            }
+        }
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            if (entry.getKey().equals(item))
+                inventory.replace(entry.getKey(), entry.getValue() - 1);
+        }
+
     }
 
     /**
@@ -39,6 +62,10 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+
+        if (inventory.containsKey(item))
+            return inventory.get(item);
+
+        return 0;
     }
 }
