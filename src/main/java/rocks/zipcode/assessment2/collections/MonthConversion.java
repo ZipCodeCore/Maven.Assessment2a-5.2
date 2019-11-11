d036bed93e8e34e24947ee5508c68f809d6f82a0
Miral Doshi <miraldoshi@Zipoders-MacBook-Pro-2.local>
2019-11-11 10:48:50 -0500

    collection 3 tests fails

--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,14 +1,24 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+    HashMap<Integer , String> months = new HashMap<Integer, String>();
+
+
+         public MonthConversion(){
+             HashMap<Integer , String> months = new HashMap<Integer, String>();
+        }
+
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
     public void add(Integer monthNumber, String monthName) {
+        months.put(monthNumber,monthName);
 
     }
 
@@ -17,14 +27,17 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+     return months.get(monthNumber);
+        //throw new NullPointerException();
     }
 
     /**
+     *
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
     public int getNumber(String monthName) {
+         // return months.get(monthName);
         return (Integer)null;
     }
 
@@ -33,7 +46,8 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+
+        return months.containsKey(monthNumber);
     }
 
     /**
@@ -41,14 +55,15 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+
+        return months.containsValue(monthName);
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return months.size();
     }
 
     /**
@@ -56,6 +71,7 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+        months.remove(monthNumber);
+        months.put(monthNumber, monthName);
     }
 }
