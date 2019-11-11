4c11ffd53393c9122e6b1166e130d4c369ba8862
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-11 09:54:26 -0500

    arrays tests pass

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -11,16 +13,16 @@ public class StringArrayUtils {
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
     public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+        return Arrays.copyOfRange(arrayToBeSpliced, startingIndex, endingIndex);
     }
 
 
-    /**
-     * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
-     * @return an array all elements between after `startingIndex`
-     */
+        /**
+         * @param arrayToBeSpliced - array to be evaluated
+         * @param startingIndex - starting index of array to be spliced
+         * @return an array all elements between after `startingIndex`
+         */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+        return Arrays.copyOfRange(arrayToBeSpliced, startingIndex, arrayToBeSpliced.length);
     }
 }
