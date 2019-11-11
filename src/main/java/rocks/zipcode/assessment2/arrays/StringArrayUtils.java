237dbf69b3557674332c902de9d9a1355dc06d1a
kendrang <kendrang@users.noreply.github.com>
2019-11-11 10:20:59 -0500

    did atm

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,26 +1,33 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 28/11/2018.
  */
 public class StringArrayUtils {
     /**
      * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
-     * @param endingIndex - ending index of array
+     * @param startingIndex    - starting index of array to be spliced
+     * @param endingIndex      - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
-    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
+    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) throws IndexOutOfBoundsException, IllegalArgumentException {
+        String[] shortArr = new String[endingIndex - startingIndex-1];
         return null;
     }
 
 
     /**
      * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
+     * @param startingIndex    - starting index of array to be spliced
      * @return an array all elements between after `startingIndex`
      */
-    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) throws IllegalArgumentException {
+        ArrayList<String> endingArr = new ArrayList<>();
+        for (int i = startingIndex ; i < arrayToBeSpliced.length ;  i ++){
+            endingArr.add(startingIndex, arrayToBeSpliced[i]);
+        }
+        return (String[]) endingArr.toArray();
     }
 }
--- src/main/java/rocks/zipcode/assessment2/atm/Account.java
+++ src/main/java/rocks/zipcode/assessment2/atm/Account.java
@@ -2,32 +2,57 @@ package rocks.zipcode.assessment2.atm;
 
 public class Account {
 
-    // this is only boiler plate. add your group's code here.
+    private Double accountBalance = 0.00;
 
-    // ********
-    //
-    // See why you needed to get ATM finished this weekend??
-    //
-    // ********
+    public Account(){
+        this.accountBalance = 0.0;
+    }
+
+    public Account(Double balance){
+        this.accountBalance = balance;
+    }
+
+    public Double getAccountBalance() {
+        return this.accountBalance;
+    }
+
+    public void setAccountBalance(Double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
     
     public Account(double v) {
+        this.accountBalance = v;
+
     }
 
     public double balance() {
-        return 0.0;
+        return accountBalance;
     }
 
     public boolean closeAccount() {
+        if (accountBalance == 0 ){
+            return true;
+        }
         return false;
     }
 
     public void deposit(double v) {
+        accountBalance = accountBalance + v;
     }
 
     public Double withdraw(double v) {
-        return 0.0;
+    if (accountBalance != 0){
+        accountBalance -= v;
+         }
+     return accountBalance;
     }
 
-    public void transfer(Account b, double v) {
+    public void transfer(Account b,  double v) {
+        Account a = new Account();
+        if (b.accountBalance != 0 && v <= accountBalance ){
+            b.withdraw(v);
+            a.deposit(v);
+        }
     }
 }
