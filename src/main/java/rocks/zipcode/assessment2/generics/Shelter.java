65019743132a98494179a26b442f1d78c2d5466f
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-11 12:57:38 -0500

    Add logic for add/remove/contains

--- src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
+++ src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
@@ -1,6 +1,11 @@
 package rocks.zipcode.assessment2.generics;
 
 
+import rocks.zipcode.assessment2.generics.ageable.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Shelter is a generic class that holds Objects that extends `Ageable`.
  * For example, if a Person and a Dog extends Ageable, then I can declare the following:
@@ -9,29 +14,35 @@ package rocks.zipcode.assessment2.generics;
  * Shelter<Dog> dogHouse = new Shelter<Dog>();
  */
 public class Shelter<_> {
+
+    List newPerson;
+
     public Shelter() {
-        throw new NullPointerException();
+        //throw new NullPointerException();
+        newPerson = new ArrayList<>();
     }
 
     /**
      * @return the number of item in the shelter
      */
     public int size() {
-        return -1;
+        return newPerson.size();
     }
 
     public void add(Object object) {
+        newPerson.add(object);
     }
 
     public Boolean contains(Object object) {
-        return null;
+        return newPerson.contains(object);
     }
 
     public void remove(Object object) {
+        newPerson.remove(object);
     }
 
     public Object get(Integer index) {
-        return null;
+        return newPerson.get(index);
     }
 
     public Integer getIndexOf(Object ageable) {
