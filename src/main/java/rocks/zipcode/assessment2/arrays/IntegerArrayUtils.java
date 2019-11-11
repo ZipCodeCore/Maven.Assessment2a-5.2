4da42c4d8d65ad4bb81781dd9d5fd7fc64b137ae
ushanil <ush.gopi@gmail.com>
2019-11-11 10:52:44 -0500

    ATM

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -10,7 +14,27 @@ public class IntegerArrayUtils {
      * @return - identical array with one additional element of `valueToBeAdded` at the end of the array
      */
     public static Integer[] add(Integer[] integerArray, Integer valueToBeAdded) {
-        return null;
+        Integer arraylength =  integerArray.length;
+        Integer[] newArray = new Integer[arraylength+1];
+        int i=0;
+        for (Integer intValue : newArray)
+        {
+
+            if (i> arraylength+1)
+            {
+                newArray[i] = valueToBeAdded;
+                i++;
+            }
+            else
+            {
+                newArray[i] = intValue;
+                i++;
+            }
+        }
+
+
+        newArray[arraylength] = valueToBeAdded;
+        return newArray;
     }
 
     /**
@@ -20,7 +44,15 @@ public class IntegerArrayUtils {
      * @return `integerArray` with `valueToBeInserted` at index number `indexToInsertAt`
      */
     public static Integer[] replace(Integer[] integerArray, int indexToInsertAt, Integer valueToBeInserted) {
-        return null;
+       /* List<Integer> list = Arrays.asList(integerArray);
+        list.add(indexToInsertAt,valueToBeInserted);
+
+        Integer arraylength =  integerArray.length;
+        Integer[] newArray = new Integer[arraylength+1]; */
+
+        integerArray[indexToInsertAt] = valueToBeInserted;
+
+        return integerArray;
     }
 
     /**
@@ -29,7 +61,7 @@ public class IntegerArrayUtils {
      * @return element located at `indexToFetch`
      */
     public static Integer get(Integer[] integerArray, Integer indexToFetch) {
-        return null;
+        return integerArray[indexToFetch];
     }
 
     /**
@@ -37,7 +69,24 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1 and odd-values decremented by 1
      */
     public static Integer[] incrementEvenDecrementOdd(Integer[] integerArray) {
-        return null;
+        Integer arraylength =  integerArray.length;
+        Integer[] newArray = new Integer[arraylength];
+
+        int i=0;
+        for (Integer intValue: integerArray) {
+            if(intValue%2 == 0)
+            {
+                newArray[i] = intValue+1;
+                i++;
+            }
+            else
+            {
+                newArray[i] = intValue-1;
+                i++;
+            }
+        }
+
+        return newArray;
     }
 
     /**
@@ -45,7 +94,21 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+        Integer arraylength =  integerArray.length;
+        Integer[] newArray = new Integer[arraylength];
+
+        int i=0;
+        for (Integer intValue: integerArray) {
+            if(intValue%2 == 0)
+            {
+                newArray[i] = intValue+1;
+                i++;
+            }
+
+        }
+
+        return newArray;
+
     }
 
     /**
@@ -53,6 +116,32 @@ public class IntegerArrayUtils {
      * @return identical array with odd-values decremented by 1
      */
     public static Integer[] decrementOdd(Integer[] input) {
-        return null;
+        Integer arraylength =  input.length;
+        Integer[] newArray = new Integer[arraylength];
+
+        int i=0;
+        for (Integer intValue: input) {
+            if(intValue%2 != 0)
+
+            {
+                newArray[i] = intValue-1;
+                i++;
+            }
+        }
+
+        return newArray;
+    }
+
+
+    //added by usha
+    public static Integer[] castingMethodtoInteger(ArrayList al)
+    {
+        Integer[] c = new Integer[al.size()];
+        for(int i = 0; i < al.size(); i++)
+        {
+            c[i] = (Integer) al.get(i);
+        }
+        return c;
+
     }
 }
