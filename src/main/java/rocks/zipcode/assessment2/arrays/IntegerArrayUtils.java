41deb391513ca7b246e1db33d5a4aaa379fa7ff3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-11 09:29:04 -0500

    Add logic for increment even

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -52,7 +52,18 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+
+        Integer[] newArray = new Integer[integerArray.length];
+        int counter =0;
+        for (Integer value : integerArray){
+            if ((value % 2 == 0)) {
+                newArray[counter] = integerArray[counter] + 1;
+            } else {
+                newArray[counter] = integerArray[counter];
+            }
+            counter++;
+        }
+        return newArray;
     }
 
     /**
