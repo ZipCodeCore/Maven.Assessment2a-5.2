23b377aa54fd1084141f5d3c8e9fd32ba56c32d3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-11 09:55:29 -0500

    Add logic for ending array

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.Map;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -11,7 +13,15 @@ public class StringArrayUtils {
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
     public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+        startingIndex = Math.abs(startingIndex);
+        endingIndex = Math.abs(endingIndex);
+        String [] result = new String[endingIndex-startingIndex];
+        int x = startingIndex;
+        for (int i = 0; i < result.length; i++){
+            result[i] = arrayToBeSpliced[x];
+            x++;
+        }
+        return result;
     }
 
 
@@ -21,6 +31,18 @@ public class StringArrayUtils {
      * @return an array all elements between after `startingIndex`
      */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+
+        if (startingIndex < 0 || startingIndex > arrayToBeSpliced.length){
+            String[] result = new String[arrayToBeSpliced.length];
+            return result;
+        } else {
+            int x = startingIndex;
+            String[] result = new String[arrayToBeSpliced.length - startingIndex];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = arrayToBeSpliced[x];
+                x++;
+            }
+            return result;
+        }
     }
 }
