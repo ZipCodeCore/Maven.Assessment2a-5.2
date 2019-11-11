347851c7b122a26bccb03e1484d4eca4aaca848a
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-11 12:36:05 -0500

    Add pad to right logic

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -22,7 +22,11 @@ public class StringUtils {
      * @return `stringToBePadded` flushed right by right-padding
      */
     public static String padRight(String stringToBePadded, int amountOfPadding) {
-        return null;
+
+        String result = "";
+
+        result = String.format("%-" + amountOfPadding + "s",stringToBePadded);
+        return result;
     }
 
     /**
