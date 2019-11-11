7051ccb77ad1084f2170f114f41168d875099b4d
MannyMonocle <eetumadu@gmail.com>
2019-11-11 10:49:32 -0500

    account 9/10

--- src/main/java/rocks/zipcode/assessment2/atm/Account.java
+++ src/main/java/rocks/zipcode/assessment2/atm/Account.java
@@ -1,33 +1,152 @@
 package rocks.zipcode.assessment2.atm;
 
+import java.util.List;
+import java.util.Random;
+
 public class Account {
 
-    // this is only boiler plate. add your group's code here.
+    private Long accountNumber;
+    private Double userPin;
+    public Double balance;
+    private int status;
+    private int overdraft;
+    //private AccountHistory accountHistory;
+    public String accountType;
 
-    // ********
-    //
-    // See why you needed to get ATM finished this weekend??
-    //
-    // ********
-    
-    public Account(double v) {
+//----------- constructor -------------------------------
+
+    public Account(Double userPin){
+
+        setAccountNumber();
+        //this.accountHistory = new AccountHistory();
+        this.userPin = userPin;
+        this.balance = userPin;
+        this.status = 0;
+        this.overdraft = 0;
+
     }
 
-    public double balance() {
-        return 0.0;
+//-------- business logic ---------------------------
+
+
+    public void deposit(Double amount){
+        this.balance += amount;
+        //accountHistory.recordHistoryDeposit(amount);
     }
 
-    public boolean closeAccount() {
+    public double withdraw(Double amount){
+        if(checkBalance(amount)) {
+            return balance -= amount;
+
+        } else
+            return 0;
+    }
+
+    public String transfer(Account transferTo, Double amount){
+        if(checkBalance(amount)){
+            this.balance = this.balance - amount;
+            transferTo.balance += amount;
+            //accountHistory.recordHistoryOutgoingTransfer(amount, transferTo.accountNumber);
+            //transferTo.accountHistory.recordHistoryIncomingTransfer(amount, this.accountNumber);
+            return null;
+        }else
+            return "Insufficient funds";
+    }
+
+    public boolean closeAccount(){
+        return userPin == 0.0;
+    }
+
+
+// -----------------------------------------------
+
+    public boolean checkBalance(Double amount){
+        if (amount < this.balance)
+            return true;
+
         return false;
     }
 
-    public void deposit(double v) {
+
+
+// ---------- setters and getters -------------------------------------
+
+    public Long getAccountNumber() {
+        return accountNumber;
     }
 
-    public Double withdraw(double v) {
-        return 0.0;
+    public void setAccountNumber() {
+        //this.accountNumber = accountNumber;
+
+        Random random = new Random(System.nanoTime());
+
+        this.accountNumber = Long.valueOf(random.nextInt(1000000000));
+
     }
 
-    public void transfer(Account b, double v) {
+   public Double balance() {
+        return balance;
     }
+
+    /*
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getUserPin() {
+        return userPin;
+    }
+
+    public void setUserPin(String pin) {
+        this.userPin = pin;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getOverdraft() {
+        return overdraft;
+    }
+
+    public void setOverdraft(int overdraft) {
+        this.overdraft = overdraft;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+
+    public String getHistoryAll(){
+        //return toString(accountHistory.getAllTransactions());
+    }
+
+    public String getHistoryDeposit(){
+        //return toString(accountHistory.getDeposits());
+    }
+
+    public String getHistoryWithdraw(){
+        //return toString(accountHistory.getWithdrawals());
+    }
+
+    public String getHistoryTransfer(){
+        //return toString(accountHistory.getAllTransactions());
+    }*/
+
+
+    public String toString(List<String> list){
+
+        return "Account: " + this.getAccountNumber() + "\n" + list.toString().replace(", ", "\n")
+                .replace("[", "").replace("]", "\n");
+    }
+
 }
