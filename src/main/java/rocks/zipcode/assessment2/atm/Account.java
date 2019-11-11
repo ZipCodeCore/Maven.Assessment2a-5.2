00be3cb053291aff57716386d401efb7aa79e2e1
Mike King <michkin32@gmail.com>
2019-11-11 10:17:49 -0500

    finished accounts

--- src/main/java/rocks/zipcode/assessment2/atm/Account.java
+++ src/main/java/rocks/zipcode/assessment2/atm/Account.java
@@ -1,7 +1,7 @@
 package rocks.zipcode.assessment2.atm;
 
 public class Account {
-
+    private double balance;
     // this is only boiler plate. add your group's code here.
 
     // ********
@@ -11,23 +11,38 @@ public class Account {
     // ********
     
     public Account(double v) {
+        this.balance = v;
     }
 
     public double balance() {
-        return 0.0;
+        return balance;
     }
 
     public boolean closeAccount() {
-        return false;
+        if (balance == 0.0){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public void deposit(double v) {
+        balance += v;
     }
 
     public Double withdraw(double v) {
+        if (balance >= v){
+            balance -= v;
+        return v;
+    }else {
         return 0.0;
+    }
     }
 
     public void transfer(Account b, double v) {
+
+
+        b.deposit(this.withdraw(v));
     }
 }
