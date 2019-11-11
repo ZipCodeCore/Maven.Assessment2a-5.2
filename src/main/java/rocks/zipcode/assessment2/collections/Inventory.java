3aace5aca64628f5e0cb263b31b57d4aa4de7555
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-11 10:23:46 -0500

    Add logic for inventory

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,16 +1,19 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+
+    List<String> newList = new ArrayList<>();
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        newList = strings;
     }
 
     /**
@@ -24,14 +27,15 @@ public class Inventory {
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        newList.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+
+        newList.remove(item);
     }
 
     /**
@@ -39,6 +43,10 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        int result = 0;
+        for (String value : newList){
+            if (newList.contains(value)) result ++;
+        }
+        return result;
     }
 }
