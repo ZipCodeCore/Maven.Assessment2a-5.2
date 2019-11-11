12e2f99d4d1c5a6b9654fc72977bbce56d9137b9
David Trombello <david@MacBook-Pro-8.localdomain>
2019-11-11 10:27:10 -0500

    several sections passing

--- src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
@@ -7,7 +7,8 @@ public class BasicStringUtils {
      * @return concatenation of `string1` and `string2`
      */
     public static String concatentate(String string1, String string2) {
-        return null;
+        
+        return string1 + string2;
     }
 
     /**
@@ -15,7 +16,9 @@ public class BasicStringUtils {
      * @return an identical string with characters in reverse order
      */
     public static String reverse(String string1) {
-        return null;
+        StringBuilder sb = new StringBuilder(string1);
+        sb.reverse();
+        return sb.toString();
     }
 
     /**
@@ -24,7 +27,8 @@ public class BasicStringUtils {
      * @return concatenation of the reverse of `string1` and reverse of `string2`
      */
     public static String reverseThenConcatenate(String string1, String string2) {
-        return null;
+        String reversed = reverse(string1) +  reverse(string2);
+        return reversed;
     }
 
     /**
@@ -33,7 +37,9 @@ public class BasicStringUtils {
      * @return `string` with `charactersToRemove` removed
      */
     public static String removeCharacters(String string, String charactersToRemove) {
-        return null;
+       StringBuilder sb = new StringBuilder(string);
+
+        return string.replaceAll("charactersToRemove", "");
     }
 
     /**
