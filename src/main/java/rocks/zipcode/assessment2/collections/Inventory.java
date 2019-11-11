8350e61331a71f762ed831401903ecbc2392e048
wes <wes.jones1597@gmail.com>
2019-11-11 10:36:06 -0500

    collections tests passing

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,39 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    private List<String> strings;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        this.strings = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        this.strings = new ArrayList<String>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        this.strings.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        this.strings.remove(item);
     }
 
     /**
@@ -39,6 +41,10 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        Integer count = 0;
+        for (String string : strings) {
+            if (string.equals(item)) count++;
+        }
+        return count;
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,15 +1,25 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+    private Map<Integer, String> months;
+    public MonthConversion() {
+        this.months = new HashMap<Integer, String>();
+    }
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
     public void add(Integer monthNumber, String monthName) {
-
+        if (!this.months.containsValue(monthName) && !this.months.containsKey(monthNumber)) {
+            this.months.put(monthNumber, monthName);
+        }
     }
 
     /**
@@ -17,15 +27,27 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+        String monthName = null;
+        if (this.months.containsKey(monthNumber)) {
+            monthName = this.months.get(monthNumber);
+        }
+        return monthName;
     }
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
-    public int getNumber(String monthName) {
-        return (Integer)null;
+    public Integer getNumber(String monthName) {
+        Integer monthNumber = null;
+        Set keys = this.months.keySet();
+        for (Object key : keys) {
+            if (this.months.get(key).equals(monthName)) {
+                monthNumber = (Integer) key;
+                break;
+            }
+        }
+        return monthNumber;
     }
 
     /**
@@ -33,7 +55,7 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        return (this.months.containsKey(monthNumber));
     }
 
     /**
@@ -41,14 +63,14 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        return (this.months.containsValue(monthName));
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return this.months.size();
     }
 
     /**
@@ -56,6 +78,7 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
+        this.months.put(monthNumber, monthName);
 
     }
 }
