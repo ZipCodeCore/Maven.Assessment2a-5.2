32856787db69d8524fe909d326b17b6078ab6f9d
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-11-11 09:59:31 -0500

    collections passing 66 tests ttl

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -2,15 +2,21 @@ package rocks.zipcode.assessment2.collections;
 
 import java.util.List;
 
+import java.util.HashMap;
+
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    HashMap<String, Integer> account = new HashMap<>();
+
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        for(String item : strings){
+            addItemToInventory(item);
+        }
     }
 
     /**
@@ -18,20 +24,30 @@ public class Inventory {
      */
     public Inventory() {
 
+
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+
+        Integer quantity = account.get(item);
+        if(quantity == null)
+            quantity = 1 ;
+        else
+            quantity = quantity + 1;
+        account.put(item, quantity);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+
+        Integer quantity = account.get(item);
+        if(quantity != null)
+            account.put(item, quantity-1);
     }
 
     /**
@@ -39,6 +55,7 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+
+        return account.getOrDefault(item,0);
     }
 }
