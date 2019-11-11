8083b2d810589855a7dbd684c17785c7d2e6fff2
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-11-11 11:11:53 -0500

    fundamentals

--- src/main/java/rocks/zipcode/assessment2/fundamentals/PredicateUtilities.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/PredicateUtilities.java
@@ -1,12 +1,18 @@
 package rocks.zipcode.assessment2.fundamentals;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 public class PredicateUtilities {
     /**
      * @param value - the value to be evaluated
      * @return true if `value` is a multiple of 2
      */
     public static Boolean isEven(Integer value) {
-        return null;
+        Boolean even = false;
+        if (value % 2 == 0) {
+            even = true;
+        }
+        return even;
     }
 
     /**
@@ -14,7 +20,11 @@ public class PredicateUtilities {
      * @return true if `value` is not a multiple of 2
      */
     public static Boolean isOdd(Integer value) {
-        return null;
+        Boolean odd = false;
+        if (isEven(value) == false) {
+            odd = true;
+        }
+        return odd;
     }
 
     /**
@@ -22,24 +32,36 @@ public class PredicateUtilities {
      * @return true if `value` is a multiple of 3
      */
     public static Boolean isMultipleOf3(Integer value) {
-        return null;
+        Boolean multi3 = false;
+        if (value % 3 == 0) {
+            multi3 = true;
+        }
+        return multi3;
     }
 
     /**
-     *
-     * @param value - the value to be evaluated
+     * @param value    - the value to be evaluated
      * @param multiple - the multiple to test `value` against
      * @return true if `value` is a multiple of `multiple`
      */
     public static Boolean isMultipleOfN(Integer value, Integer multiple) {
-        return null;
+        Boolean multi = false;
+        if (value % multiple == 0) {
+            multi = true;
+        }
+        return multi;
     }
+
 
     /**
      * @param string - the string to be evaluated
      * @return true if `string` starts with a capital letter
      */
     public static Boolean startsWithCapitalLetter(String string) {
-        return null;
+        Boolean cap = false;
+        if (Character.isUpperCase(string.charAt(0))) {
+            cap = true;
+        }
+        return cap;
     }
 }
