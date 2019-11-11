2d965c925cb8a8d27e50209218c47d5afe7e03af
ushanil <ush.gopi@gmail.com>
2019-11-11 09:50:12 -0500

    Generics and ObjectOriented

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -5,6 +5,11 @@ package rocks.zipcode.assessment2.objectorientation;
  */
 public class Address {
     public Address() {
+        this.addLine1 = "";
+        this.addLine2 = "";
+        this.city = "";
+        this.state = "";
+        this.zipcode = "";
     }
 
     /**
@@ -14,46 +19,63 @@ public class Address {
      * @param state - state of city
      * @param zipcode - zipcode of region
      */
+    private String addLine1;
+    private String addLine2;
+    private String city;
+    private String state;
+    private String zipcode;
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
+        this.addLine1 = addressLine1;
+        this.addLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
     }
 
     public String getAddressLine1() {
-        return null;
+        return this.addLine1;
     }
 
     public void setAddressLine1(String addressLine1) {
+        this.addLine1 = addressLine1;
+
     }
 
-    public String getAddressLine2() {
-        return null;
-    }
+    public String getAddressLine2(){ return this.addLine2;
+}
 
     public void setAddressLine2(String addressLine2) {
+        this.addLine2 = addressLine2;
     }
 
     public String getCity() {
-        return null;
+        return this.city;
     }
 
     public void setCity(String city) {
+        this.city = city;
     }
 
     public String getState() {
-        return null;
+        return this.state;
     }
 
     public void setState(String state) {
+        this.state = state;
     }
 
     public String getZipcode() {
-        return null;
+        return this.zipcode;
     }
 
     public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        Boolean compareObjects = false;
+
+        return (Boolean)compareObjects;
     }
 }
--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -9,33 +9,58 @@ public class Person {
      * @param id - id of person
      * @param name - name of person
      * @param address - address of person
+     *
      */
+
+    private Long personId;
+    private String personName;
+    private Address personAddress;
     public Person(Long id, String name, Address address) {
+        this.personName = name;
+        this.personId = id;
+        this.personAddress = address;
     }
 
     public Person() {
+        this.personName = "";
+        this.personId = Long.MIN_VALUE;
+        this.personAddress = new Address();
     }
 
     public Long getId() {
-        return null;
+        return this.personId;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id) {this.personId = id;
     }
 
     public String getName() {
-        return null;
+        return this.personName;
     }
 
-    public void setName(String name) {
+    public void setName(String name) {this.personName = name;
     }
 
     public Address getAddress() {
-        return null;
+    return this.personAddress;
     }
 
-    public void setAddress(Address address) {
+    public void setAddress(Address address) {this.personAddress = address;
+
     }
+
+    public String toString(){
+        String addressLine1 = this.personAddress.getAddressLine1();
+        String addressLine2 = this.personAddress.getAddressLine2();
+        String city = this.personAddress.getCity();
+        String state = this.personAddress.getState();
+        String zipcode = this.personAddress.getZipcode();
+
+        StringBuilder address = new StringBuilder();
+        address.append(String.format("Address{addressLine1='%s', addressLine2='%s', city='%s', state='%s', zipcode='%s'",addressLine1,addressLine2,city,state,zipcode));
+        return address.toString();
+    }
+
 
     @Override
     public boolean equals(Object o) {
