20ceb5e3424b13e9b14dc96593facfbca622b411
michael-doll <MWDoll88@gmail.com>
2019-11-11 12:09:26 -0500

    add Inventory, passing

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,50 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    Map<String, Integer> inv;
+    private List<String> strings;
+
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        this.strings = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+    this.inv = new HashMap<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        if(inv.containsKey(item)){
+            inv.put(item,inv.get(item)+1);
+        }else{
+            inv.put(item,1);
+        }
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        if(inv.containsKey(item)){
+            inv.put(item,inv.get(item)-1);
+        }
+
     }
 
     /**
@@ -39,6 +52,6 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        return inv.getOrDefault(item, 0);
     }
 }
